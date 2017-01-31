35ca0ebd978b1317344037b0853a1e93216879df
HiDoYa <hiroyagojo@gmail.com>
2017-01-31 09:56:59 -0800

    Started working on enemy class

--- include/Enemy.cpp
+++ include/Enemy.cpp
@@ -3,3 +3,38 @@
 Enemy::Enemy()
 {
 }
+
+void setPosition(int x, int y)
+{
+	sprite.setPosition(sf::Vector2f(x, y));
+}
+
+void setScale(int sc)
+{
+	sprite.setScale(sf::Vector2f(sc, sc));
+}
+
+void setHealth(int i)
+{
+	health = i;
+}
+
+void setAttackDmg(int i)
+{
+	attackDmg = i;
+}
+
+sf::Sprite getSprite()
+{
+	return sprite;
+}
+
+int getHealth()
+{
+	return health;
+}
+
+int getAttackDmg()
+{
+	return attackDmg;
+}
--- include/Enemy.h
+++ include/Enemy.h
@@ -5,8 +5,20 @@
 
 class Enemy 
 {
-	public:
 	private:
+		sf::Sprite sprite;
+		sf::Texture texture;
+		int health;
+		int attackDmg;
+	public:
+		Enemy();
+		void setPosition(int, int);
+		void setScale(int);
+		void setHealth(int);
+		void setAttackDmg(int);
+		sf::Sprite getSprite();
+		int getHealth();
+		int getAttackDmg();
 }
 
 #endif
