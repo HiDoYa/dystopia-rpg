a3aa43040e0836f8753f89c43c84019e3a2f2c90
HiDoYa <hiroyagojo@gmail.com>
2017-01-30 18:15:40 -0800

    Started on tree class

---
+++ include/Tree.cpp
@@ -0,0 +1,6 @@
+#include "Tree.h"
+#include "Environment.h"
+
+Tree::Tree()
+{
+}
---
+++ include/Tree.h
@@ -0,0 +1,14 @@
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+
+#ifndef TREE_H
+#define TREE_H
+
+class Tree
+{
+	private:
+	public:
+		Tree();
+}
+
+#endif
