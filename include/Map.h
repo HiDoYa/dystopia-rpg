401c2077b89e2fba7ee81cfa71c2b74f5bc0af74
HiDoYa <hiroyagojo@gmail.com>
2017-02-06 11:55:34 -0800

    Created map class

--- include/Map.h
+++ include/Map.h
@@ -1,6 +1,23 @@
-#include <iostream>
+#include <memory>
+#include <SFML/Graphics.hpp>
 
 #ifndef MAP_H
 #define MAP_H
+
+class Map
+{
+	private:
+		sf::Sprite sprite;
+		sf::Texture texture;
+	public:
+		Map(sf::String);
+		void backgroundRender(sf::RenderWindow);
+		void midgroundRender(sf::RenderWindow);
+		std::unique_ptr<sf::RectangleShape[]> midgroundCollision(int);
+		void midgroundCollisionBoxSet(int, int, int, int, int);
+		void midgroundFrontRender(sf::RenderWindow);
+		void foregroundRender(sf::RenderWindow);
+		void allMapRender(sf::RenderWindow);
+}
 
 #endif
