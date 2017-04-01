4f86320fc4ac0365ec213e976fb3f721637daf2c
HiDoYa <hiroyagojo@gmail.com>
2017-03-31 18:58:39 -0700

    started on map menu

---
+++ include/MapMenu.cpp
@@ -0,0 +1,8 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+#include "MapMenu.h"
+
+MapMenu::MapMenu()
+{
+}
---
+++ include/MapMenu.h
@@ -0,0 +1,10 @@
+#ifndef MAPMENU_H
+#define MAPMENU_H
+
+class MapMenu
+{
+	private:
+	public:
+};
+
+#endif
